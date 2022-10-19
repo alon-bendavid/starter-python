119634e2288c989d9c0cc841a3c1e15238115a57
alon-bendavid <114388870+alon-bendavid@users.noreply.github.com>
2022-10-19 13:49:06 +0200

    Add files via upload

---
+++ job18.py
@@ -0,0 +1,14 @@
+#function to organise numbers from lowest to greatest
+
+def myList(*arg):
+
+#organise numbers from lower to higher
+    x = sorted(arg)
+    print(x)
+    
+
+myList(3 , 100 , 14 , 40 , 35)
+
+
+
+
