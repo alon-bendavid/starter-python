3f394a087693bb6e584d898b4321f512880fe3bd
alon-bendavid <114388870+alon-bendavid@users.noreply.github.com>
2022-10-19 23:12:26 +0200

    Add files via upload

---
+++ job19.py
@@ -0,0 +1,18 @@
+
+def listFunc(*arg):
+ my_list = arg
+
+ for i in range(len(my_list)):
+    print(range(len(my_list)))
+    for j in range(i + 1, len(my_list)):
+
+        if my_list[i] > my_list[j]:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+
+    print(my_list)
+
+listFunc(6 , 4 , 5 , 11 , 35 , 2)    
+
+    
+
+
---
+++ job20.py
@@ -0,0 +1,7 @@
+def myAddition( num1 , num2):
+    return(num1 + num2)
+
+
+
+print(myAddition(1 , 3))
+    
